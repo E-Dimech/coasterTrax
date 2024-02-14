906950b64a543db8835c9dd4cbb52697a3583e8b
Eric Dimech <ericdimech@hotmail.com>
2024-02-14 12:53:32 -0500

    updated home imports++

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchCoasters from "../Search/Search";
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Coaster Search testing</h1>
+      <SearchCoasters />
+    </div>
+  );
+}
+
+export default Home;
